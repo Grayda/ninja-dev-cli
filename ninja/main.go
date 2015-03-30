af5ba7ad0b114a19073a971bda27af993e0ebc28
Theo Julienne <theo.julienne@gmail.com>
2015-03-30 13:32:49 -0700

    Remove unimplemented option.

--- ninja/main.go
+++ ninja/main.go
@@ -8,12 +8,10 @@ const usage = `Ninja Sphere Developer CLI.
 
 Usage:
   ninja build <path>
-  ninja image
   ninja -h | --help
 
 Options:
   build         Builds a package for a driver or app, ready for distribution to the Ninja Sphere.
-  image         Creates a complete image based on Ubuntu Core Snappy.
   -h --help     Show this screen.
 `
 
